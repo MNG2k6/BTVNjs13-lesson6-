4411a9e17b1ef34cb955352730f0982d14430867
MNG2k6 <87935209+MNG2k6@users.noreply.github.com>
2021-09-11 17:14:13 +0700

    BTVN buổi 6

---
+++ index.js
@@ -0,0 +1,56 @@
+// Bai 1
+
+// function student(name, age, address){
+//     const student1 = {
+//         name: "Khoa",
+//         age: "15",
+//         address: "69 Tran Duy Hung",
+//     };
+//     console.log(student1);
+// }
+
+// student();
+
+// Bai 2
+
+// var smartPhone = [
+//     {name:'iphone', price: 649},
+//     {name:'Galaxy S6', price: 576},
+//     {name:'Galaxy Note 5', price: 489},
+// ];
+
+// function price() {
+//     for (let index = 0; index < smartPhone.length; index++) {
+//         if(index === 0){
+//             console.log(smartPhone[index].price);
+//         } else if(index === 1){
+//             console.log(smartPhone[index].price);
+//         } else {
+//             console.log(smartPhone[index].price);
+//         }
+//     };
+// };
+
+// price();
+
+// Bai 3
+
+// const foo = (x, y, z) => {
+//     console.log(`${x}, ${y}, ${z}`);
+// };
+
+// foo();
+
+// const foo = () => {
+//     alert(`Hello`);
+// };
+
+// foo();
+
+// const foo = (a, b) =>{
+//     let m = a + (b*100) ;
+//     return m;
+// };
+
+// foo();
+
